c19e8960591e0ee7fa839b8a34ae7e12f6e55c4c
Gianmarco Simone <gianmarcosimone89@gmail.com>
2020-11-29 19:02:55 +0100

    Adds sdf example

--- example/index.tsx
+++ example/index.tsx
@@ -3,6 +3,7 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
 const Main = React.lazy(() => import('./pages/main'))
+const SDF = React.lazy(() => import('./pages/sdf'))
 
 import { Switch, Route } from 'wouter'
 
@@ -14,6 +15,9 @@ function App() {
       <Switch>
         <Route path="/">
           {() => <Main />}
+        </Route>
+        <Route path="/sdf">
+          {() => <SDF />}
         </Route>
       </Switch>
     </React.Suspense>
